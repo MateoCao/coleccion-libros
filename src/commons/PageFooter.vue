37421e44bc0251010de986b8397337a56beb9fb1
Mateo Cao <mateocao12@gmail.com>
2023-09-18 15:06:39 -0300

    Icon

--- src/commons/PageFooter.vue
+++ src/commons/PageFooter.vue
@@ -1,6 +1,11 @@
 <template>
-  <footer class="bg-black h-16 mt-5 text-white flex justify-center md:justify-end items-center p-2">
-    <div class="text-3xl flex gap-8">
+  <footer class="bg-black min-h-[65px] mt-5 text-white flex justify-center flex-wrap md:justify-between items-center p-2 gap-2">
+    <div>
+      <p class="text-lg">
+        Copyright © BiblioTrack
+      </p>
+    </div>
+    <div class="text-2xl md:text-3xl flex gap-8">
       <a href="https://github.com/MateoCao/coleccion-libros">
         <font-awesome-icon :icon="['fab', 'github']" />
       </a>
