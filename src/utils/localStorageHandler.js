d1b009b864f3d96108aaff54b7f77e5a59d47c92
Mateo Cao <mateocao12@gmail.com>
2023-09-18 13:53:37 -0300

    localstorage handler

---
+++ src/utils/localStorageHandler.js
@@ -0,0 +1,22 @@
+const STORAGE_KEY = 'books'
+
+export default {
+  getBooks() {
+    try {
+      const booksStorage = localStorage.getItem(STORAGE_KEY);
+      return JSON.parse(booksStorage) || [];
+    } catch (error) {
+      console.error('Error al obtener los libros del localStorage:', error);
+      return [];
+    }
+  },
+
+  setBooks(books) {
+    try {
+      const booksStorage = JSON.stringify(books);
+      localStorage.setItem(STORAGE_KEY, booksStorage);
+    } catch (error) {
+      console.error('Error al guardar los libros en el localStorage:', error);
+    }
+  },
+}
